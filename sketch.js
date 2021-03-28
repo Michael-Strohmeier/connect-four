742d535e2ce8b366fad1966feaa2c76771f1c54e
Michael <46147963+Michael-Strohmeier@users.noreply.github.com>
2021-03-28 18:27:30 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -4,12 +4,11 @@ function setup() {
 }
 
 function mousePressed() {
-  game.move(4);
-  print("moved 4");
+  game.move(mouseX);
 }
 
 function draw() {
-  background(220);
+  background(240, 235, 220, 255);
   game.draw();
 }
 
@@ -26,10 +25,33 @@ class Piece {
     this.r = r;
     this.color = c;
     this.isAnimating = false;
+    
+    this.centers = [];
+  }
+  
+  setCenters(centers) {
+    this.centers = centers;
+  }
+  
+  mouseToX(x) {
+    let closestDist = 1000000;
+    let index = 0;
+    for (let i = 0; i < this.centers.length; i++) {
+      if (abs(this.centers[i] - x) < closestDist) {
+        closestDist = abs(this.centers[i] - x);
+        index = 6-i;
+      }
+    }
+    
+    return index;
   }
   
   setY() {
     
+  }
+  
+  setColor(c) {
+    this.color = c;
   }
   
   getY() {
@@ -37,6 +59,7 @@ class Piece {
   }
   
   updateX(x) {
+    
     this.pos.x = x;
   }
   
@@ -44,7 +67,15 @@ class Piece {
     push();
     noStroke();
     fill(this.color);
-    ellipse(this.pos.x, this.pos.y, this.r);
+    translate(this.pos.x, this.pos.y);
+    rectMode(CENTER);
+    
+    rotate(PI/ 4);
+    rect(-8, 0, 24, 8);
+    
+    rotate(PI/ 2);
+    rect(-8, 0, 24, 8);
+    
     pop();
   }
 }
@@ -69,7 +100,11 @@ class BoardDrawer {
   }
   
   getColumnCenters() {
-    
+    let centers = [];
+    for (let j = 0; j < this.board[0].length; j++) {
+        centers.push(windowWidth / 2 - j * this.r * this.spaceFactor + 3 * this.r * this.spaceFactor);
+    }
+    return centers;
   }
   
   getTopY() {
@@ -79,14 +114,35 @@ class BoardDrawer {
   
   
   draw() {
+    
+    push();
+    noStroke();
+    fill(63, 145, 241, 240);
+    rectMode(CENTER);
+    let centX = windowWidth / 2 - 3 * this.r * this.spaceFactor + 3 * this.r * this.spaceFactor;
+    let centY = windowHeight / 2 - 2.5 * this.r * this.spaceFactor + 3 * this.r * this.spaceFactor;
+    rect(centX, centY, this.r * 8, this.r * 7);
+    pop();
     for (let i = 0; i < this.board.length; i++) {
       let y = windowHeight / 2 - i * this.r * this.spaceFactor + 3 * this.r * this.spaceFactor;
 
       for (let j = 0; j < this.board[i].length; j++) {
-        push();
-        strokeWeight(2);
+        
+        
+        
+        
+        
         
         let x = windowWidth / 2 - j * this.r * this.spaceFactor + 3 * this.r * this.spaceFactor;
+        
+        push();
+        noStroke();
+        fill(253, 247, 232, 255);
+        ellipse(x, y, this.r - 1);
+        pop();
+        
+        push();
+        strokeWeight(2);
         if (this.board[5 - i][6 - j] == 1) {
           fill(216,77,106);
         } else if (this.board[5 - i][6 - j] == 2) {
@@ -143,9 +199,14 @@ class Game {
                   [0, 0, 0, 0, 0, 0, 0],
                   [0, 0, 0, 0, 0, 0, 0]];
     // color(221,172,82)
+    
+    // need to set centers again on window resize
+    this.currPiece.setCenters(this.boardDrawer.getColumnCenters()); 
   }
   
-  move(columnNumber) {
+  move(x) {
+    let columnNumber = this.currPiece.mouseToX(x);
+    
     let dropRow = -1;
     if (this.board[0][columnNumber] == 0) {
       // legal column
@@ -171,6 +232,13 @@ class Game {
         this.boardDrawer.setBoard(this.board);
         
         this.state = State.IDLE;
+        if (this.turn == Turn.PLAYER_ONE) {
+          this.turn = Turn.PLAYER_TWO;
+          this.currPiece.setColor(color(221,172,82));
+        } else {
+          this.turn = Turn.PLAYER_ONE;
+          this.currPiece.setColor(color(216,77,106));
+        }
       }
     } else {
       this.currPiece.updateX(mouseX);
